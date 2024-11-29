3aa8d5d5d14f5ea051283b2fc6be6dc5c6e951b5
Anja <anja.jankovic@elfak.rs>
2024-11-29 19:35:08 +0100

    razvlacnje gumica i detekcija trouglica

--- Trigle.py
+++ Trigle.py
@@ -1,6 +1,8 @@
 import pygame
 import math
 
+koordinate = []
+
 def nacrtaj_tablu(x, space, start):
     c = 1
     r = 0
@@ -13,7 +15,6 @@ def nacrtaj_tablu(x, space, start):
     z1 = 0
     h=(x-2)*10
     p1 = start - h
-    koordinate = []
 
     while n != 2*x-1:
         while r != 2*x-1:
@@ -41,9 +42,7 @@ def nacrtaj_tablu(x, space, start):
                 i += 1
                 r += 1
         n += 1
-
-    graf = napravi_graf(koordinate)
-    return graf, koordinate
+    return koordinate
 
 def napravi_graf(koordinate):
     graf = { }
@@ -81,11 +80,48 @@ def validan_potez(stub1, stub2, space):
         return False
     return True
 
-print(validan_potez((100, 40), (60, 100), 20))
+def razvuci_gumicu(stub1, stub2, graf, space):
+    #c1 i c2 su stubici izmedju koji moraju da se uzmu u obzir
+    if validan_potez(stub1, stub2, space):
+        if proveri_pravac(stub1, stub2, space) == "D":
+            c1 = ((stub1[0]+space), stub1[1])
+            c2 = ((stub1[0] + 2* space), stub1[1])
+        if proveri_pravac(stub1, stub2, space) == "DL":
+            c1=((stub1[0]-(space//2)), (stub1[1]+space))
+            c2=((stub1[0]-space), (stub1[1]+2*space))
+        if proveri_pravac(stub1, stub2, space) == "DD":
+            c1=((stub1[0]+(space//2)), (stub1[1]+space))
+            c2 = ((stub1[0] + space), (stub1[1] + 2 * space))
+        graf[stub1].append(c1)
+        graf[c1].append(stub1)
+        graf[c1].append(c2)
+        graf[c2].append(c1)
+        graf[c2].append(stub2)
+        graf[stub2].append(c2)
+
+        pygame.draw.line(win, BLACK, stub1, c1, 2)
+        pygame.draw.line(win, BLACK, c1, c2, 2)
+        pygame.draw.line(win, BLACK, c2, stub2, 2)
+
+def detektuj_trougao(stub1, stub2, graf, space):
+    if stub1 in graf[stub2]:
+        for stub3 in graf[stub1]:
+            if stub3 in graf[stub2] and stub3 != stub1:
+                pygame.draw.polygon(win, RED, [stub1, stub2, stub3])
+                return True
+    return False
+
+
+def pocetno_stanje(n):
+    space = 20
+    start = 100
+    nacrtaj_tablu(n, space, start)
+
 
 screen = WIDTH, HEIGHT = 600, 600
 
 WHITE = (255, 255, 255)
+RED = (252, 91, 122)
 BLACK = (12, 12, 12)
 
 pygame.init()
@@ -99,7 +135,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    nacrtaj_tablu(6, 20, 100)
+    pocetno_stanje(6)
+    graf = napravi_graf(koordinate)
+    #razvuci_gumicu((100, 40), (70, 100), graf, 20)
+    #razvuci_gumicu((100, 40), (130, 100), graf, 20)
+    #razvuci_gumicu((90, 60), (150, 60), graf, 20)
+    #razvuci_gumicu((100, 40), (160, 40), graf, 20)
+    #razvuci_gumicu((120, 40), (90, 100), graf, 20)
+    #detektuj_trougao((100, 40), (90, 60), graf, 20)
+    #detektuj_trougao((100, 40), (120, 40), graf, 20)
     pygame.display.update()
 
 pygame.quit()
